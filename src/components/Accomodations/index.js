f7e1de0cbf05a1241e8f83165c989221770c9400
RazvanAnisia <anisiarazvan@gmail.com>
2019-10-07 21:44:10 +0100

    added table

--- src/components/Accomodations/index.js
+++ src/components/Accomodations/index.js
@@ -2,12 +2,98 @@ import React from 'react';
 
 import parse from 'html-react-parser';
 import ReactPaginate from 'react-paginate';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 import * as data from '../../utils/accommodation_data.json';
 
 function Accomodations() {
+  const newTable = data.accommodations.map(accommodation => {
+    return {
+      acc_details: [
+        accommodation.name,
+        accommodation.resort.name,
+        parse(accommodation.description),
+        [
+          accommodation.address_1,
+          accommodation.address_2,
+          accommodation.address_3,
+          accommodation.country.name,
+          accommodation.postcode
+        ],
+        accommodation.location['@location_lat'],
+        accommodation.location['@location_long']
+      ],
+      //acc_facilities: accommodation.facilities,
+      acc_rating: accommodation.rating.label[0],
+      acc_type: accommodation.type.name
+
+      //
+    };
+  });
+
+  console.log(newTable);
+  const columns = [
+    {
+      Header: ' Accommodation',
+      accessor: 'acc_details',
+      render: accommodation => (
+        <>
+          <h4>{accommodation[0]}</h4>
+          <p>Resort: {accommodation[1]}</p>
+          <p>{accommodation[2]}</p>
+          {accommodation[3].map(el => (
+            <span>{el + ' '} </span>
+          ))}
+          {/* <ul>
+            {accommodation[3].length > 0 &&
+              accommodation[3].map(facility => <li>{facility}</li>)}
+          </ul> */}
+          <a
+            href={`https://www.google.com/maps/search/?api=1&query=${
+              accommodation[5]
+            },${accommodation[6]}`}
+            target="_blank">
+            Directions
+          </a>
+        </>
+      )
+    },
+    {
+      Header: 'Rating',
+      accessor: 'acc_rating'
+    },
+    {
+      Header: 'Type',
+      accessor: 'acc_type'
+    },
+    {
+      Header: 'Facilities',
+      accessor: 'acc_facilities'
+    },
+    {
+      Header: 'Directions',
+      accessor: 'acc_directions',
+      render: value => (
+        <a
+          href={`https://www.google.com/maps/search/?api=1&query=${
+            value['@location_lat']
+          },${value['@location_long']}`}
+          target="_blank">
+          Directions
+        </a>
+      )
+    }
+  ];
   return (
     <div>
+      <ReactTable
+        defaultPageSize={10}
+        minRows={1}
+        data={newTable}
+        columns={columns}
+      />
+
       {data.accommodations.map(accomodation => (
         <div>
           <h4>{accomodation.name}</h4>
